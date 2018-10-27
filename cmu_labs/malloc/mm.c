a866dd1abc5107ac60c594e777941261d547a14e
giulio <giuliop@gmail.com>
2018-10-27 18:25:12 +0200

    started working on it

--- cmu_labs/malloc/mm.c
+++ cmu_labs/malloc/mm.c
@@ -41,7 +41,7 @@ team_t team = {
 /* rounds up to the nearest multiple of ALIGNMENT */
 #define ALIGN(size) (((size) + (ALIGNMENT-1)) & ~0x7)
 
-#define WSIZE = 4;
+#define WSIZE 4
 
 #define SIZE_T_SIZE (ALIGN(sizeof(size_t)))
 
