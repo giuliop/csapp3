3622c59e7c00d83fdbb72abd3910c8e872b7c7ee
giulio <giuliop@gmail.com>
2018-12-29 23:12:33 +0100

    Make proxy work :)

--- cmu_labs/proxy/proxy.c
+++ cmu_labs/proxy/proxy.c
@@ -14,26 +14,38 @@ struct req_hdrs {
 	char others[MAXLINE];
 };
 
-void set_def_req_hdrs(struct req_hdrs * rh) {
-	strcpy(rh->user_agent, "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 Firefox/10.0.3\r\n");
-	strcpy(rh->connection, "Connection: close\r\n");
-	strcpy(rh->proxy_connection, "Proxy_connection: close\r\n");
+struct http10request {
+	char method[MAXLINE];
+	char host[MAXLINE];
+	char port[MAXLINE];
+	char path[MAXLINE];
+	char version[MAXLINE];
+	char req_line[MAXLINE];
+	char post_arg[MAXLINE];
+	struct req_hdrs * hdrs;
+};
+
+void set_def_req_field(struct http10request * r) {
+	strcpy (r->version, "HTTP/1.0");
+	strcpy(r->hdrs->user_agent, "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 Firefox/10.0.3\r\n");
+	strcpy(r->hdrs->connection, "Connection: close\r\n");
+	strcpy(r->hdrs->proxy_connection, "Proxy_connection: close\r\n");
 }
 
 void doit(int fd);
 void read_requesthdrs(rio_t * rp, struct req_hdrs * rh);
-void serve(int fd, char *filename, int filesize, char * method);
-void get_filetype(char *filename, char *filetype);
-void get_host(char * uri, char * dest);
+void read_post_params(rio_t * rio, struct http10request * req);
+void send_request(struct http10request * r, int clientfd);
+void parse_uri(char * uri, char * host, char * port, char * path);
 void clienterror(int fd, char * cause, char *errnum, 
 		char * shortmsg, char *longmsg);
 void * thread_f (void * x);
-void test_get_host();
+void test();
+
 
 int main(int argc, char **argv) 
 {
-	test_get_host();
-
+	test();
 	int listenfd, connfd;
 	char hostname[MAXLINE], port[MAXLINE];
 	socklen_t clientlen;
@@ -93,29 +105,75 @@ void doit(int fd)
 		return;
 	}
 
-	/*long content_len = 0;*/
-	/*read_requesthdrs(&rio, method, &content_len);*/
-	struct req_hdrs rh = {"", "", "", "", ""};
+	struct req_hdrs rh = { "", "", "", "", "" };
+	struct http10request req = { "", "", "", "", "", "", "", &rh };
 	read_requesthdrs(&rio, &rh);
-	set_def_req_hdrs(&rh);
+	strcpy(req.method, method);
+	set_def_req_field(&req);
 
+	parse_uri(uri, req.host, req.port, req.path);
 	if (rh.host[0] == '\0') {	
-		char h[MAXLINE];
-		get_host(uri, h);
-		if (h[0] == '\0') {
-			clienterror(fd, buf, "400", "Bad uri", "The uri is not valid");
+		if (req.host[0] == '\0') {
+			clienterror(fd, buf, "400", "Bad uri", "Cannot determine valid host");
 			return;
 		}
-		strcpy(rh.host, "Host: ");
-		strcat(h, "\r\n");
-		strcat(rh.host, h);
+		int n = snprintf(rh.host, MAXLINE, "Host: %s:%s\r\n", req.host, req.port);
+		if (n >= MAXLINE) {
+			clienterror(fd, buf, "400", "Bad host", "Host name is too long");
+			return;
+		}
 	}
-	printf("\nmethod: %s\nuri: %s \nversion: %s\n", method, uri, version);
-	printf("\n%s%s%s%s%s\n", rh.host, rh.user_agent, rh.connection,
-	rh.proxy_connection, rh.others);
+	if (req.path[0] == '\0') {	
+			clienterror(fd, buf, "400", "Bad uri", "Cannot determine valid path");
+			return;
+	}
+	int n = snprintf(req.req_line, MAXLINE, "%s %s %s\r\n",
+			req.method, req.path, req.version);
+	if (n >= MAXLINE) {
+		clienterror(fd, buf, "400", "Bad request", "Request line is too long");
+		return;
+	}
 
-	/*request(...);*/
-	/*serve(...);*/
+	/*printf("\nRequest line: %s", req.req_line);*/
+	/*printf("\nRequest headers:\n%s%s%s%s%s\n", rh.host, rh.user_agent,*/
+			/*rh.connection, rh.proxy_connection, rh.others);*/
+
+	if (! strcmp(method, "POST"))
+			read_post_params(&rio, &req);
+
+	// Send the request to the server
+	int clientfd = Open_clientfd("localhost", "4000");
+	/*int clientfd = Open_clientfd(req.host, req.port);*/
+	if (clientfd < 0) {
+		clienterror(fd, buf, "400", "Bad request", "Cannot contact server");
+		return;
+	}
+	send_request(&req, clientfd);
+
+	// Read response from server and send to the client
+	char resp[MAX_OBJECT_SIZE];
+	Rio_readinitb(&rio, clientfd);
+	int read;
+	while ((read = Rio_readnb(&rio, resp, MAX_OBJECT_SIZE)) > 0)
+		Rio_writen(fd, resp, read);
+		
+	Close(clientfd);
+}
+
+void send_request(struct http10request * r, int clientfd) 
+{
+	Rio_writen(clientfd, r->req_line, strlen(r->req_line));
+	Rio_writen(clientfd, r->hdrs->host, strlen(r->hdrs->host));
+	Rio_writen(clientfd, r->hdrs->user_agent, strlen(r->hdrs->user_agent));
+	Rio_writen(clientfd, r->hdrs->connection, strlen(r->hdrs->connection));
+	Rio_writen(clientfd, r->hdrs->proxy_connection,
+			strlen(r->hdrs->proxy_connection));
+	Rio_writen(clientfd, r->hdrs->others, strlen(r->hdrs->others));
+	Rio_writen(clientfd, "\r\n", 2);
+
+	if (! strcmp (r->method, "POST")) {
+		Rio_writen(clientfd, r->post_arg, strlen(r->post_arg));
+	}
 }
 
 // read_requesthdrs - read HTTP request headers and fill req_hdrs with
@@ -148,85 +206,65 @@ void read_requesthdrs(rio_t *rp, struct req_hdrs * rh)
 	}
 }
 
-// Extract the host from the input uri and copy it to dest
-// Return empty string if not successful
-void get_host(char * uri, char * dest)
+// Read the request body of POST requests and save it in the input req struct
+void read_post_params(rio_t * rio, struct http10request * req)
+{
+	char * i = strstr(req->hdrs->others, "Content-Length");
+	if (! i)
+		return;
+	i += strlen("Content-Length: ");
+	char * j = index(i, '\r');
+	if (! j)
+		return;
+	char len[MAXLINE];
+	memcpy(len, i, j-i);
+	len[j-i] = '\0';
+	int content_len = strtol(len, NULL, 10);
+	Rio_readnb(rio, req->post_arg, content_len);
+}
+
+// Extract the host and path from the input uri and copy it to the
+// input pointers; return empty strings if not successful
+void parse_uri(char * uri, char * host, char * port, char * path)
 {
 // supported uri types are the following two, with () indicating
 // optional elements:
 //		1. scheme://(userinfo@)host(:port)(/(path))
 //		2. path
-// we only consider success if url is of type 1 and we can extract a host
-	char * i = strstr(uri, "://");
-	if ( (! i) || (i[3] == '\0') ) {
-		dest[0] = '\0';
+	char * scheme_i = strstr(uri, ":");
+	char * host_i = strstr(uri, "://");
+	if (! host_i) {
+		host[0] = '\0';
+		port[0] = '\0';
+		if (! scheme_i)
+			strncpy(path, uri, MAXLINE);
+		else
+			strncpy(path, scheme_i + 1, MAXLINE);
 		return;
 	}
 
-	char * user = index(i, '@'); 
-	char * host = user ? user + 1 : i + 3;
+	char * user_i = index(host_i, '@'); 
+	host_i = user_i ? user_i + 1 : host_i + 3;
 
-	char * path = index(host, '/');
-	if (path) {
-		int host_len = path - host;
-		strncpy(dest, host, host_len);
-		*(dest + host_len) = '\0';
+	char * path_i = index(host_i, '/');
+	if (path_i) {
+		int host_len = path_i - host_i;
+		strncpy(host, host_i, host_len);
+		*(host + host_len) = '\0';
+		strncpy(path, path_i, MAXLINE);
 	}
-	else
-		strcpy(dest, host);
-
-	char * port = index(host, ':');
-	if (! port) {
-		strcpy(dest + strlen(dest), ":80");
+	else {
+		strncpy(host, host_i, MAXLINE);
+		strcpy(path, "/");
 	}
-}
 
-
-/* serve- copy response back to the client */
-void serve(int fd, char *filename, int filesize, char *method) 
-{
-	int srcfd;
-	char *srcp, filetype[MAXLINE], buf[MAXBUF];
-
-	/* Send response headers to client */
-	get_filetype(filename, filetype);       
-	sprintf(buf, "HTTP/1.0 200 OK\r\n");    
-	sprintf(buf, "%sServer: Tiny Web Server\r\n", buf);
-	sprintf(buf, "%sConnection: close\r\n", buf);
-	sprintf(buf, "%sContent-length: %d\r\n", buf, filesize);
-	sprintf(buf, "%sContent-type: %s\r\n\r\n", buf, filetype);
-	Rio_writen(fd, buf, strlen(buf));       
-	printf("Response headers:\n");
-	printf("%s", buf);
-
-	if (! strcasecmp(method, "HEAD"))
-		return;
-
-	/* Send response body to client */
-	srcfd = Open(filename, O_RDONLY, 0);    
-	srcp = malloc(filesize);
-	Rio_readn(srcfd, srcp, filesize);
-	Close(srcfd);                           
-	Rio_writen(fd, srcp, filesize);         
-}
-
-/* get_filetype - derive file type from file name */
-void get_filetype(char *filename, char *filetype) 
-{
-	if (strstr(filename, ".html"))
-		strcpy(filetype, "text/html");
-	else if (strstr(filename, ".css"))
-		strcpy(filetype, "text/css");
-	else if (strstr(filename, ".gif"))
-		strcpy(filetype, "image/gif");
-	else if (strstr(filename, ".png"))
-		strcpy(filetype, "image/png");
-	else if (strstr(filename, ".jpg"))
-		strcpy(filetype, "image/jpeg");
-	else if (strstr(filename, ".mpg"))
-		strcpy(filetype, "video/mpeg");
-	else
-		strcpy(filetype, "text/plain");
+	char * port_i = index(host, ':');
+	if (! port_i)
+		strcpy(port, "80");
+	else {
+		strncpy(port, port_i + 1, MAXLINE);
+		*port_i = '\0';
+	}
 }
 
 /* clienterror - returns an error message to the client */
@@ -234,6 +272,7 @@ void clienterror(int fd, char *cause, char *errnum,
 		char *shortmsg, char *longmsg) 
 {
 	char buf[MAXLINE], body[MAXBUF];
+	printf("\nERROR\n");
 
 	/* Build the HTTP response body */
 	sprintf(body, "<html><title>Tiny Error</title>");
@@ -253,29 +292,61 @@ void clienterror(int fd, char *cause, char *errnum,
 }
 
 
-void test_get_host()
+void test_parse_uri()
 {
-	char * in[] = { " http://giulio@www.abc.xyz.com"				,
+	char * uris[] = { "http://giulio@www.abc.xyz.com"				,
 									 "http://www.abc.xyz.com"								,
 									 "http://www.abc.xyz.com:30"						,
 									 "http://www.abc.xyz.com/path/path"			,
 									 "http://www.abc.xyz.com:30/path/path"	,
 									 "www.abc.xyz.com:30/path/path"					,
+									 "path@/path"														,
 	};
-	char * out[] = {  "www.abc.xyz.com:80"	,
-									  "www.abc.xyz.com:80"	,
-										"www.abc.xyz.com:30"	,
-										"www.abc.xyz.com:80"	,
-										"www.abc.xyz.com:30"	,
+	char * hosts[] = {"www.abc.xyz.com"	,
+									  "www.abc.xyz.com"	,
+										"www.abc.xyz.com"	,
+										"www.abc.xyz.com"	,
+										"www.abc.xyz.com"	,
+										""										,
 										""										,
 	};
-	for (int i = 0; i < 6; ++i) {
-		char dest[MAXBUF];
-		get_host(in[i], dest);
-		/*printf("Test %d -> testing %40s\n"*/
-					 /*"          wanting %40s\n"*/
-					 /*"          getting %40s\n\n", i, in[i], out[i], dest);*/
-		assert(! strcmp(out[i], dest));
+	char * ports[] = {"80"	,
+									  "80"	,
+										"30"	,
+										"80"	,
+										"30"	,
+										""		,
+										""		,
+	};
+	char * paths[] = { "/"							,
+									   "/"							,
+										 "/"							,
+										 "/path/path"			,
+										 "/path/path"			,
+										 "30/path/path"		,
+										 "path@/path"			,
+	};
+	for (int i = 0; i < 7; ++i) {
+		char host[MAXBUF];
+		char path[MAXBUF];
+		char port[MAXLINE];
+		parse_uri(uris[i], host, port, path);
+		/*printf("Test %d -> testing uri %40s\n"*/
+				/*"          wanting host%40s\n"*/
+				/*"          getting host%40s\n"*/
+				/*"          wanting port%40s\n"*/
+				/*"          getting port%40s\n"*/
+				/*"          wanting path%40s\n"*/
+				/*"          getting path%40s\n\n",*/
+				/*i+1, uris[i], hosts[i], host, ports[i], port, paths[i], path);*/
+		assert(! strcmp(hosts[i], host));
+		assert(! strcmp(ports[i], port));
+		assert(! strcmp(paths[i], path));
 	}
+}
+
+void test()
+{
+	test_parse_uri();
 }
 
