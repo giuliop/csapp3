e93b361150ca8381109b81b3a9dbc5142fa6ccbe
giulio <giuliop@gmail.com>
2019-01-05 23:04:13 +0100

    fix wrong type for val field of item struct

--- cmu_labs/proxy/cache.h
+++ cmu_labs/proxy/cache.h
@@ -6,21 +6,21 @@
 struct item
 {
 	long size;			// the size in bytes of the item
-	char * val;			// the actual value
+	char val[];			// the actual value
 };
 
 
 // lookup a key in the cache, return NULL if not found or a pointer to 
 // the item struct otherwise
-struct item * cache_lookup(char * key)
+struct item * cache_lookup(char * key);
 
 // insert an item in the cache, evicting the lru item(s) if we exceed
 // the cache size; will overwrite the key if present
 // size is the size in bytes of the supplied val
-void cache_insert(char * key, char * val, long size)
+void cache_insert(char * key, char * val, long size);
 
 // initialize the cache; return 1 if success, 0 otherwise
-int cache_init(long maxsize, long hashcount)
+int cache_init(long maxsize, long hashcount);
 
 // reset the cache, will require to reinitialize it
 void cache_reset();
